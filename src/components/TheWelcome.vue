5840f6ae7418381c6805811e407b8879e14252e3
Bohdan Leonets <leonetsbogdan@gmail.com>
2023-11-04 18:16:25 +0200

    add transition group + gsap animation

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,12 +1,40 @@
 <script setup>
+import { computed, ref } from "vue";
 import { shuffleArray } from "@/helpers";
-import { ref } from "vue";
+import { gsap } from "gsap";
 
 const pokemon = ref([]);
+const search = ref('');
+const matches = computed(() => {
+  return pokemon.value.filter((p) => p.name.toLowerCase().includes(search.value.toLowerCase()));
+});
 
 fetch('https://pokeapi.co/api/v2/pokemon?limit=7').then(async (res) => {
   pokemon.value = (await res.json()).results;
 });
+
+function onBeforeEnter(el) {
+  el.style.opacity = 0;
+  el.style.height = 0;
+}
+
+function onEnter(el, done) {
+  gsap.to(el, {
+    opacity: 1,
+    height: '1.6em',
+    delay: el.dataset.index * 0.1,
+    onComplete: done,
+  });
+}
+
+function onLeave(el, done) {
+  gsap.to(el, {
+    opacity: 0,
+    height: 0,
+    delay: el.dataset.index * 0.1,
+    onComplete: done,
+  });
+}
 
 const add = () => {
   pokemon.value = [...pokemon.value, {name: `${new Date().getTime()}`}];
@@ -19,9 +47,21 @@ const add = () => {
       Home
     </h1>
 
+    <input v-model="search" type="search">
+
     <div v-if="pokemon.length">
-      <TransitionGroup tag="ul">
-        <li v-for="(creature, i) in pokemon" :key="creature.name">
+      <TransitionGroup
+        tag="ul"
+        :css="false"
+        @before-enter="onBeforeEnter"
+        @enter="onEnter"
+        @leave="onLeave"
+      >
+        <li
+          v-for="(creature, i) in matches"
+          :key="creature.name"
+          :data-index="i"
+        >
           {{ creature.name }}
           <button @click="pokemon.splice(i, 1)">X</button>
         </li>
