29e8cde5ee9dc26a5f216e26a84aefcd226209cf
Bohdan Leonets <leonetsbogdan@gmail.com>
2023-11-04 14:13:00 +0200

    customisation styles of transition classes

--- src/views/TransitionsView.vue
+++ src/views/TransitionsView.vue
@@ -1,10 +1,25 @@
 <script setup>
-
+import { ref } from "vue";
+const showMenu = ref(false);
 </script>
 
 <template>
   <div class="transitions">
     <h1>Transitions</h1>
+
+    <div class="transitions__menu">
+      <button v-wave class="transitions__menu-btn" @click="showMenu = !showMenu">
+        {{ showMenu ? 'Hide' : 'Show'}} Menu
+      </button>
+
+      <Transition>
+        <ul v-if="showMenu" class="transitions__list">
+          <li>Item 1</li>
+          <li>Item 2</li>
+          <li>Item 3</li>
+        </ul>
+      </Transition>
+    </div>
   </div>
 </template>
 
@@ -14,4 +29,27 @@
   flex-direction: column
   align-items: center
   justify-content: center
+
+  &__menu
+    min-height: 100%
+
+  &__menu-btn
+    padding: 10px 20px
+    margin-bottom: 20px
+    min-width: 200px
+
+  &__list
+    &.v-enter-active,
+    &.v-leave-active
+      transition: opacity 0.5s ease, transform 0.5s ease
+
+
+    &.v-enter-from
+      opacity: 0
+      transform: translateY(-20px)
+
+    &.v-leave-to
+      opacity: 0
+      transform: translateY(20px)
+
 </style>
